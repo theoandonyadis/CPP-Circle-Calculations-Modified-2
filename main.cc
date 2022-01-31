6530c702ccbce516c20863cf3629c644a361357d
theoandonyadis <77030091+theoandonyadis@users.noreply.github.com>
2022-01-30 20:41:33 -0500

    Update main.cc

--- main.cc
+++ main.cc
@@ -1,6 +1,6 @@
 //Theo Andonyadis
-//ECE 2620 Section 001
-//Lab 3
+/*This program uses various functions to make calculations based on the center point and a point on the outside of a circle entered by the user. It differs from
+CPP-Circle-Calculations in that it uses objects and classes as well as public, private, and protected members. It differs from the first modified version in that it employs templating*/
 #include <iostream>
 #include <cmath>
 #include <limits>
